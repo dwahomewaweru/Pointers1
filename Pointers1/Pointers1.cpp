d6cc1dd5af29249399b89f66ae69cdbe3cc34716
Wahome97 <d.wahome@alustudent.com>
2021-11-26 03:04:52 +0300

    Add project files.

---
+++ Pointers1/Pointers1.cpp
@@ -0,0 +1,15 @@
+// Pointers1.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int* ptr;
+	int num;
+	num = 7;
+	ptr = &num;
+
+	cout << "The pointer for the variable run is : ";
+	cout << ptr << endl;
+}
